7c1cca2f3eaab3951989e2a423dc5f7377c57c04
GweeKentYee <kentyee2001@hotmail.my>
2021-12-11 01:26:17 +0800

    Created BallFactory for Ball Model

---
+++ src/test/BallFactory.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.awt.geom.Point2D;
+
+public class BallFactory {
+
+    public Ball makeBall(String ballType, Point2D ballPosition){
+
+        if (ballType == null){
+            return null;
+        }
+        if (ballType.equals("RUBBERBALL")){
+            return new RubberBall(ballPosition);
+        }
+        return null;
+    }
+    
+}
