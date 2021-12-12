57ec608983832bf4becacff63386a6cb1695621c
GweeKentYee <kentyee2001@hotmail.my>
2021-12-12 21:20:08 +0800

    Created GameFrameController for GameFrame

---
+++ src/test/GameFrameController.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+public class GameFrameController implements WindowFocusListener {
+
+    private GameFrame gameFrame;
+
+    public GameFrameController(GameFrame gameFrame){
+
+        this.gameFrame = gameFrame;
+
+    }
+
+    @Override
+    public void windowGainedFocus(WindowEvent e) {
+        
+        gameFrame.setGaming(true);
+        
+    }
+
+    @Override
+    public void windowLostFocus(WindowEvent e) {
+        
+        if (gameFrame.isGaming()){
+
+            gameFrame.getGameBoard().onLostFocus();
+
+        }
+        
+    }
+
+}
