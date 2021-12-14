47f70e10025c0cd81df477e3150f8d3413179156
GweeKentYee <kentyee2001@hotmail.my>
2021-12-14 13:17:05 +0800

    Removed unused import from Leaderboard

--- src/Game/Model/Leaderboard.java
+++ src/Game/Model/Leaderboard.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 import org.json.JSONArray;
